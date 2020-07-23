5e256cb00b5836a2e62df01ccf31db4057ded850
Nikita Nikolaev <nikolaevnik13@gmail.com>
2020-07-23 21:31:47 +0300

    1111

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.endpoints = [
+            "https://propets-searcher.herokuapp.com",
+
+            "https://propets-notification-com.herokuapp.com",
+
+            "https://propets-converter-for-notific.herokuapp.com",
+
+            "https://propets-favourites-activity.herokuapp.com",
+
+            "https://propets-dispatcher.herokuapp.com/",
+
+
+            "https://propets-app.herokuapp.com/",
+
+            "https://propets-messaging.herokuapp.com/",
+
+            "https://propets-config-server.herokuapp.com/",
+
+            "https://propets-lostfound.herokuapp.com/",
+
+            "https://protest-accounting.herokuapp.com/"
+
+        ]
+    }
+    fetchToHeroku=()=>{
+
+    this.endpoints.forEach(e=>{
+
+            fetch(e).then();
+    })
+    };
+
+    render() {
+        return (
+
+            <button onClick={this.fetchToHeroku}
+                    type="button">wake up my services!</button>
+
+
+        )
+    }
+
+
 }
 
 export default App;
